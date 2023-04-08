0d1ac1d97ab95cb77291935edebdc3cca5e99474
AlishaJoy <alishajoy.2002@gmail.com>
2023-04-08 22:41:37 +0530

    complete

--- ba.py
+++ ba.py
@@ -2,6 +2,9 @@ print('hello all')
 
 a = 'Learn Python Basics'
 b = " welcome to the class"
+c = a + ',' + b
+c = ' ' + c + ' are you excited!'
+d = '{} by step by step processs'.format(a)
 print(a)
 print(a[9])
 print(a[6:])
@@ -12,4 +15,6 @@ print(b.upper())  #uppercase
 print(a.count('n')) #counts the repeatation
 print(b.find('o')) #finds the position
 print(a.replace('Python','Python language'))
+print(c)
+print(d)
 
