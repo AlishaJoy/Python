98e654b89b0986e3c793f7b1a4c7e21fb69e987c
AlishaJoy <alishajoy.2002@gmail.com>
2023-04-08 22:05:59 +0530

    oo

--- ba.py
+++ ba.py
@@ -1,2 +1,15 @@
+print('hello all')
 
+a = 'Learn Python Basics'
+b = " welcome to the class"
+print(a)
+print(a[9])
+print(a[6:])
+print(len(b))
+print(b[0:8])
+print(a.lower()) #lowercase
+print(b.upper())  #uppercase
+print(a.count('n')) #counts the repeatation
+print(b.find('o')) #finds the position
+print(a.replace('Python','Python language'))
 
