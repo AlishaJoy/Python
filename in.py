7122de73a089b5455a92936f75e45529167bc6c9
AlishaJoy <alishajoy.2002@gmail.com>
2023-04-30 13:06:09 +0530

    nneww

--- in.py
+++ in.py
@@ -1,1 +1,26 @@
 #integers and floats
+w = 10
+print(type(w))
+
+#arithmetic operations:
+print(10 + 2) #addition
+print(3 - 20) #subtraction
+print(4 * 2) #multipliation
+print(33 / 4) #division
+print(44 % 2) #modulus
+print(3 // 5) #floor division
+print(3 ** 2) #exponent
+
+print(99 * 9 + 1)
+
+#using variables 
+w += 22
+print(w)
+
+print(abs(-22))
+print(round(55.88))
+print(round(99.333,2))
+
+#comparisons
+
+
