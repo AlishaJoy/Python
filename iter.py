0e82f7c259f30958c6ed5adcd595e1843e112cbd
Alishajoy <alishajoy.2002@gmail.com>
2023-05-04 19:45:22 +0530

    complete

--- iter.py
+++ iter.py
@@ -24,6 +24,34 @@ for i in num:
     for eachletter in 'pqr':
         print(i,eachletter)
         
+#for a loop till a range 
+for i in range(5):
+      print(i)
+
+for i in range(1,3):
+    print(i)
+    
+#while loops  -- until certain condition is met
+
+e = 0
+while e<8:
+    if e == 5:
+        break
+    print(e)
+    e +=1 
+ 
+f = 0
+while True:
+    if f == 4:
+        break
+    print(e)
+    e += 1
+    
+    
+    
+
+    
+               
         
             
         
