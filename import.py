19ad59c4e325ac8a87c9c8c2b167e5e38ff95aad
Alishajoy <alishajoy.2002@gmail.com>
2023-05-12 19:45:42 +0530

    ok

--- import.py
+++ import.py
@@ -1,9 +1,14 @@
  #import modules
- 
+import sys
 dictionary = ['hey','hi','bye']
 
-import imported
+import imported as im
 
-use = imported.index(dictionary,'hi')
+use = im.index(dictionary,'hi')
 print(use)
+L = [1,2,3,4]
 
+#def select_second(L):
+#or we can use ---- from imported import 
+
+#print(sys.path)
